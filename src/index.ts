94ae9acc077d00b451e48c6ecd7383b4a660df1e
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-24 16:40:51 +0100

    dev: added nodemon

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+/*
+GET / HTTP/2.0
+User-Agent: Chrome
+
+HTTP/1.1 200 OK
+Content-Length: 12
+Content-Type: text/plain
+
+Hello World!
+*/
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
