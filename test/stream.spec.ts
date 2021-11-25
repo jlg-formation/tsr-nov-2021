a3c778b9a30e00fc2578e5a5e84d06ae57e472ec
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-25 12:28:41 +0100

    dev: added stream test

--- test/stream.spec.ts
+++ test/stream.spec.ts
@@ -19,40 +19,36 @@ describe("CSV", () => {
     // Note: we use the crlfDelay option to recognize all instances of CR LF
     // ('\r\n') in input.txt as a single line break.
 
+    let headerFields = [];
+
     for await (const line of rl) {
       // Each line in input.txt will be successively available here as `line`.
       console.log(`Line from file: ${line}`);
+
+      if (line === "") {
+        continue;
+      }
+
+      if (headerFields.length === 0) {
+        headerFields = line.split(",");
+        continue;
+      }
+
+      const fields = line.split(",");
+
+      const json = {};
+      for (let i = 0; i < headerFields.length; i++) {
+        const name = headerFields[i];
+        json[name] = fields[i];
+      }
+
+      const id = fields[0];
+      await fs.promises.writeFile(
+        `./temp/json-output/employe-${id}.json`,
+        JSON.stringify(json, undefined, 2)
+      );
     }
 
-    // const unixCsvContent = csvContent.replace(/\r/g, "");
-
-    // // split by line
-    // const array = unixCsvContent.split("\n");
-
-    // const filterArray = array.filter((line) => line !== "");
-
-    // const dataArray = filterArray.slice(1);
-
-    // const header = filterArray[0];
-
-    // const headerFields = header.split(",");
-
-    // // split by field
-    // for (const elt of dataArray) {
-    //   const fields = elt.split(",");
-
-    //   const json = {};
-    //   for (let i = 0; i < headerFields.length; i++) {
-    //     const name = headerFields[i];
-    //     json[name] = fields[i];
-    //   }
-
-    //   const id = fields[0];
-    //   await fs.promises.writeFile(
-    //     `./temp/json-output/employe-${id}.json`,
-    //     JSON.stringify(json, undefined, 2)
-    //   );
-    // }
     // create one json by line
     // create one file per json
   });
